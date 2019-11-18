5f9bddc1a46d2f37d3413e96842d1076c7945c34
Jonathan Metzgar <microwerx@outlook.com>
2019-11-17 15:02:16 -0900

    consolidate ssphh projects

---
+++ ssphh-tests/ssphh-tests.cpp
@@ -0,0 +1,13 @@
+// ssphh-tests.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fluxions_gte.hpp>
+
+#pragma comment(lib, "fluxions-gte.lib")
+
+int main()
+{
+	Fluxions::TestFluxionsGTE();
+	return 0;
+}
