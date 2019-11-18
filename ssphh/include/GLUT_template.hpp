30a066631984e0c6a5b2b13ea1b9bb0510d5610f
Jonathan Metzgar <microwerx@outlook.com>
2019-11-17 17:44:31 -0900

    Make FREEGLUT optional

--- ssphh/include/GLUT_template.hpp
+++ ssphh/include/GLUT_template.hpp
@@ -3,15 +3,19 @@
 
 #include <viperfish.hpp>
 
-void GlutTemplateSetParameters(const std::string &windowTitle, int width, int height, int displayMode = -1);
-void GlutTemplateInit(int argc, char **argv);
+//#define SSPHH_USE_FREEGLUT
+
+#ifdef SSPHH_USE_FREEGLUT
+void GlutTemplateSetParameters(const std::string& windowTitle, int width, int height, int displayMode = -1);
+void GlutTemplateInit(int argc, char** argv);
 void GlutTemplateWidget(Vf::Widget::SharedPtr widget);
 void GlutTemplateMainLoop();
 
-void PrintString9x15(float x, float y, int justification, const char *format, ...);
-void PrintBitmapStringJustified(float x, float y, int justification, void *font, const char *format, ...);
-void PrintStrokeStringJustified(float x, float y, int justification, void *font, const char *format, ...);
+void PrintString9x15(float x, float y, int justification, const char* format, ...);
+void PrintBitmapStringJustified(float x, float y, int justification, void* font, const char* format, ...);
+void PrintStrokeStringJustified(float x, float y, int justification, void* font, const char* format, ...);
 
 extern Vf::Widget::SharedPtr gt_Widget;
+#endif // SSPHH_USE_FREEGLUT
 
 #endif
