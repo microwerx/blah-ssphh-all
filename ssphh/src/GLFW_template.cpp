7143b51eb0d3678da527c17d129f14193c4b05d6
Jonathan Metzgar <jmetzgar@outlook.com>
2020-01-11 22:28:16 -0900

    add hint for GLFW for Core Profile

--- ssphh/src/GLFW_template.cpp
+++ ssphh/src/GLFW_template.cpp
@@ -256,7 +256,9 @@ bool GlfwTemplateInit(int argc, char **argv)
 
 	// monitor is set to NULL
 	// share is set to NULL
-	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwt::pGlfwWindow = glfwCreateWindow(glfwt::screenWidth, glfwt::screenHeight, glfwt::windowTitle.c_str(), NULL, NULL);
 	if (!glfwt::pGlfwWindow)
 	{
